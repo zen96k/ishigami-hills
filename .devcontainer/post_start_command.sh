5ddb3f1d5aa22fc28c8e9e9d19168c8d276f32a3
zen96k <jolly96k@gmail.com>
2024-09-23 07:07:18 +0900

    chore: npm-check-updatesをインストールするように修正

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -9,3 +9,5 @@ rm -rf ${HOME}/.gitconfig
 git config --global init.defaultBranch ${GIT_INIT_DEFAULT_BRANCH}
 git config --global user.name ${GIT_USER_NAME}
 git config --global user.email ${GIT_USER_EMAIL}
+
+npm install -g npm-check-updates
