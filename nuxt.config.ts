7b75ac29bbf9c46318ed511fc71dd26c62d83822
zen96k <jolly96k@gmail.com>
2024-09-23 14:35:15 +0900

    chore: Tailwind CSSをインストール
    https://tailwindcss.com/docs/guides/nuxtjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: false,
-  spaLoadingTemplate: true
+  spaLoadingTemplate: true,
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
