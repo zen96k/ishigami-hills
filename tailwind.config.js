f6e066ae6658a4d56d5aa324ca805eb033fc4283
zen96k <jolly96k@gmail.com>
2024-09-23 14:40:38 +0900

    chore: Tailwind CSSの公式プラグインをインストール
    https://tailwindcss.com/docs/plugins#official-plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,10 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries")
+  ]
 }
