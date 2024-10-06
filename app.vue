d856c419641706b527b544df9e095234893bc5ca
zen96k <jolly96k@gmail.com>
2024-10-06 18:19:38 +0900

    refactor: app.vueを修正

--- app.vue
+++ app.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Html
+      lang="ja"
+      data-theme="sunset"
+    >
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Html>
   </div>
 </template>
+
+<script setup lang="ts">
+  useHead({
+    titleTemplate: (pageTitle) => {
+      return pageTitle === undefined ? "石上ヒルズ" : `石上ヒルズ - ${pageTitle}`
+    }
+  })
+</script>
+
+<!-- <style>
+  @import url("https://fonts.googleapis.com/css2?family=DotGothic16&display=swap");
+
+  body {
+    font-family: "DotGothic16", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+</style> -->
