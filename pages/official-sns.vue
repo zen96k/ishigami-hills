00a108e88816624f8c95b60318c238ceb279e4a9
zen96k <jolly96k@gmail.com>
2024-10-21 04:07:44 +0900

    refactor: 公式Twitterのフィードを埋め込んで表示

--- pages/official-sns.vue
+++ pages/official-sns.vue
@@ -1,7 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <a
+      class="twitter-timeline"
+      href="https://twitter.com/zen96k/lists/1847740575807914084?ref_src=twsrc%5Etfw"
+      data-chrome="nofooter noborders"
+      >A Twitter List by zen96k</a
+    >
+  </div>
 </template>
 
 <script setup lang="ts">
-  useHead({ title: "公式SNS" })
+  useHead({
+    title: "公式SNS",
+    script: [{ src: "https://platform.twitter.com/widgets.js", async: true }]
+  })
 </script>
