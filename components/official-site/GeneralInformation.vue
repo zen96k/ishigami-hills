2b34c12bad06fd3d249505d9dc728e6411562475
zen96k <jolly96k@gmail.com>
2024-10-29 20:43:31 +0900

    refactor: 公式のRSSフィードの表示を修正

--- components/official-site/GeneralInformation.vue
+++ components/official-site/GeneralInformation.vue
@@ -6,7 +6,7 @@
           <span>一般情報</span>
           <!-- <span class="badge badge-warning">未実装</span> -->
         </h2>
-        <span>公式のRSSフィード。</span>
+        <span>公式サイトの一般情報。</span>
       </div>
       <div
         v-if="status === 'success'"
