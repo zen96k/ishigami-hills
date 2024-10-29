c40bccf412f29622b14d5db82fee8a2fd60621ab
zen96k <jolly96k@gmail.com>
2024-10-29 21:07:08 +0900

    add: ゴッチのNoteを追加

--- pages/radar/media-information.vue
+++ pages/radar/media-information.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="mx-3 my-3 grid grid-cols-1 gap-3 lg:grid-cols-3"></div>
+    <div class="mx-3 my-3 grid grid-cols-1 gap-3 lg:grid-cols-3">
+      <MediaInformationNoteGotchRSS />
+    </div>
   </div>
 </template>
 
