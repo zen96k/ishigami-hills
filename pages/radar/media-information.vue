86bc6cec92418f96ae97c258f1229b6ed99d7bc3
zen96k <jolly96k@gmail.com>
2024-10-31 00:24:36 +0900

    refactor: メディア情報の表示を修正

--- pages/radar/media-information.vue
+++ pages/radar/media-information.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="mx-3 my-3 grid grid-cols-1 gap-3 lg:grid-cols-2">
+    <div class="mx-3 my-3 grid grid-cols-1 gap-3">
       <MediaInformationNoteGotchRSS />
       <MediaInformationNoteAppleVinegarMusicSupportRSS />
       <MediaInformationNoteGoogleNewsRSS />
