8707e56a92753fddfd118224fd65c974048a9dd2
zen96k <jolly96k@gmail.com>
2024-11-10 04:24:29 +0900

    refactor: GitHub Actionsで環境変数を参照できるように修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   modules: ["@nuxt/eslint"],
   runtimeConfig: {
     public: {
-      googleMapApiKey: process.env["NUXT_PUBLIC_GOOGLE_MAP_API_KEY"] ?? ""
+      googleMapApiKey: ""
     }
   }
 })
