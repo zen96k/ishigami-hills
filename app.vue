2f17ef35d043b5e67df6c12f7190f5ea5190b82c
zen96k <jolly96k@gmail.com>
2024-11-10 16:33:04 +0900

    refactor: useSeoMetaでmeta要素を追加

--- app.vue
+++ app.vue
@@ -20,4 +20,12 @@
     },
     link: [{ rel: "icon", href: "/favicon.png" }]
   })
+  useSeoMeta({
+    title: "石上ヒルズ",
+    ogTitle: "石上ヒルズ",
+    description: "アジカンの非公式ファンサイト。",
+    ogDescription: "アジカンの非公式ファンサイト。",
+    ogImage: "https://nuxt.com/assets/design-kit/logo-green-white.png",
+    twitterCard: "summary_large_image"
+  })
 </script>
