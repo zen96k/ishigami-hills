eceb0d5b2531f33c922059e026084ea2c1c9da9a
zen96k <jolly96k@gmail.com>
2024-11-15 03:01:34 +0900

    refactor: OGPの定義を更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,19 @@ export default defineNuxtConfig({
         {
           name: "twitter:card",
           content: "summary"
+        },
+        {
+          name: "twitter:title",
+          content: "石上ヒルズ"
+        },
+        {
+          name: "twitter:description",
+          content: "アジカンの非公式ファンサイト。"
+        },
+        {
+          name: "twitter:image",
+          content:
+            "https://cdn.prod.website-files.com/603c87adb15be3cb0b3ed9b5/670dcf30efc008ddd6d9027d_061-min.png"
         }
       ]
     }
