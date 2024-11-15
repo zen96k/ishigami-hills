c9c8688b72f0d85c4419d43024b20f46d69cc5d0
zen96k <jolly96k@gmail.com>
2024-11-16 08:40:27 +0900

    refactor: head要素を更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,13 +37,17 @@ export default defineNuxtConfig({
           content: "アジカンの非公式ファンサイト。"
         },
         {
+          name: "twitter:creator",
+          content: "@zen96k"
+        },
+        {
           name: "twitter:image",
           content:
             "https://cdn.prod.website-files.com/603c87adb15be3cb0b3ed9b5/670dcf30efc008ddd6d9027d_061-min.png"
         },
         {
           name: "twitter:card",
-          content: "summary_large_image"
+          content: "summary"
         }
       ]
     }
