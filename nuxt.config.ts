b6d628acb42c7f6f1113f3050618456244cbf6b3
zen96k <jolly96k@gmail.com>
2024-11-16 08:30:31 +0900

    refactor: head要素を更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,8 +20,13 @@ export default defineNuxtConfig({
           content: "アジカンの非公式ファンサイト。"
         },
         {
+          property: "og:site_name",
+          content: "石上ヒルズ"
+        },
+        {
           property: "og:image",
-          content: "https://nuxt.com/assets/design-kit/logo-green-white.png"
+          content:
+            "https://cdn.prod.website-files.com/603c87adb15be3cb0b3ed9b5/670dcf30efc008ddd6d9027d_061-min.png"
         },
         {
           name: "twitter:title",
@@ -33,7 +38,8 @@ export default defineNuxtConfig({
         },
         {
           name: "twitter:image",
-          content: "https://nuxt.com/assets/design-kit/logo-green-white.png"
+          content:
+            "https://cdn.prod.website-files.com/603c87adb15be3cb0b3ed9b5/670dcf30efc008ddd6d9027d_061-min.png"
         },
         {
           name: "twitter:card",
