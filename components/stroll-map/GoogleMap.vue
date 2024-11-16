ae17b437530151fbe66b4f72c504a2d956b46b78
zen96k <jolly96k@gmail.com>
2024-11-16 11:21:38 +0900

    refactor: お散歩マップを更新

--- components/stroll-map/GoogleMap.vue
+++ components/stroll-map/GoogleMap.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div
-      id="map"
-      :displayMarkerElementContent="displayMarkerElementContent"
-    />
+    <div id="map" />
   </div>
 </template>
 
@@ -21,40 +18,11 @@
     mapTypeControl: false
   }
 
-  const displayMarkerElementContent = ref(false)
-
-  const toggleInitialMarkerElementContent = (
-    advancedMarkerElement: google.maps.marker.AdvancedMarkerElement
-  ) => {
-    if (!displayMarkerElementContent.value) {
-      advancedMarkerElement.content = buildInitialMarkerElementContent()
-    } else {
-      advancedMarkerElement.content = null
-    }
-    displayMarkerElementContent.value = !displayMarkerElementContent.value
-  }
-
-  const buildInitialMarkerElementContent = () => {
+  const createInfoWindowContent = () => {
     const divElement = document.createElement("div")
     divElement.innerHTML = `
     <div class="card bg-base-100 max-w-96">
       <div class="card-body">
-        <div class="card-actions justify-end">
-          <button class="btn btn-square btn-xs bg-red-800">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
         <h2 class="card-title">関東学院大学</h2>
         <span>
           ASIAN KUNG-FU GENERATIONが結成された場所。
@@ -72,11 +40,11 @@
   onMounted(async () => {
     const appHeaderElement = document.getElementById(
       "app-header"
-    ) as HTMLDivElement
+    ) as HTMLElement
     const appFooterElement = document.getElementById(
       "app-footer"
-    ) as HTMLDivElement
-    const mapElement = document.getElementById("map") as HTMLDivElement
+    ) as HTMLElement
+    const mapElement = document.getElementById("map") as HTMLElement
 
     mapElement.style.height =
       window.innerHeight -
@@ -85,21 +53,22 @@
       "px"
 
     const { Map } = await googleMap.importLibrary("maps")
-    const { AdvancedMarkerElement } = await googleMap.importLibrary("marker")
 
     const map = new Map(
       document.getElementById("map") as HTMLElement,
       mapOptions
     )
 
-    const initialMarkerElement = new AdvancedMarkerElement({
-      map: map,
-      position: initialPosition,
-      title: "関東学院大学"
-    })
-
-    initialMarkerElement.addListener("click", () => {
-      toggleInitialMarkerElementContent(initialMarkerElement)
+    map.addListener("click", (event: google.maps.MapMouseEvent) => {
+      if ("placeId" in event) {
+        event.stop()
+        console.dir(event)
+        const infoWindow = new google.maps.InfoWindow({
+          content: createInfoWindowContent(),
+          position: event.latLng
+        })
+        infoWindow.open({ map: map })
+      }
     })
   })
 </script>
