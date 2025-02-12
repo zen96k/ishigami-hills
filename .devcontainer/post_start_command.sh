0bbfe78e6a633d210fc009d38e4b0d6461aaa27e
zen96k <jolly96k@gmail.com>
2025-02-13 03:48:12 +0900

    chore: 開発コンテナを起動する時に使用していないコンテナを削除するように処理を更新

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -10,6 +10,8 @@ git config --global init.defaultBranch ${GIT_INIT_DEFAULT_BRANCH}
 git config --global user.name ${GIT_USER_NAME}
 git config --global user.email ${GIT_USER_EMAIL}
 
+docker system prune -af --volumes
+
 cd ${FRONTEND_DIRNAME}
 npm install -g npm-check-updates
 npm install
