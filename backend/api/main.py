1aed1832585a901fdf4f0fa235107ca2989374ea
zen96k <jolly96k@gmail.com>
2025-02-15 08:28:21 +0900

    refactor: RSSを取得するAPIのレスポンスのモデルを追加

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,11 @@
 import requests
 from fastapi import FastAPI, HTTPException
+from models.rss2json import rss
 
 app = FastAPI()
 
 
-@app.get(path="/rss/general-information")
+@app.get(path="/rss/general-information", response_model=rss.RSS)
 async def get_general_information():
     api_endpoint_url = "https://api.rss2json.com/v1/api.json"
     rss_url = "https://www.sonymusic.co.jp/xml/artInfoRSS.php?id=72000340"
