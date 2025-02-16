3b9939a64b573d10b2b597d0013bcbd039d9955b
zen96k <jolly96k@gmail.com>
2025-02-17 01:25:47 +0900

    chore: shadcn/vueを追加

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,3 @@
-import tailwindcss from "@tailwindcss/vite"
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -7,7 +5,16 @@ export default defineNuxtConfig({
   runtimeConfig: {
     fastApiBaseUrl: ""
   },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
-  css: ["~/assets/css/main.css"],
-  vite: { plugins: [tailwindcss()] }
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui"
+  }
 })
