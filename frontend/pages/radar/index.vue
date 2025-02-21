2d4c82e38775fb4fd732232c8665db2d99527ad2
zen96k <jolly96k@gmail.com>
2025-02-22 05:01:28 +0900

    refactor: RSSを表示するパネルの順番を更新

--- frontend/pages/radar/index.vue
+++ frontend/pages/radar/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="grid gap-4 lg:grid-cols-2 2xl:grid-cols-3">
+      <RssGoogleNews />
       <RssGeneralInformation />
       <RssGotchNote />
       <RssAVMSNote />
-      <RssGoogleNews />
     </div>
   </div>
 </template>
